f88e8b249c53cf423f38c705fb586b204ff6dbb0
bennettem <emily.pamelabennett@gmail.com>
2022-07-04 16:06:31 -0500

    server routes set up

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const PORT = 3001;
+
+const api = require("./routes/apiRoutes/apiRoutes");
+const html = require("./routes/htmlRoutes/htlmRoutes");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json);
+app.use(express.static("public"));
+app.use("/api", api);
+app.use("/", html);
+
+app.listen(PORT, () => {
+  console.log(`API server now on ${PORT}`);
+});
